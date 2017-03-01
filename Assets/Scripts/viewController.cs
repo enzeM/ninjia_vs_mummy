281aa5384ad52aa6d7642f434b4bc05e31d0c672
Ivarlock/Alienware <ivarlock.liu@gmail.com>
2017-03-01 15:17:23 +0000

    add viewController script

---
+++ Assets/Scripts/viewController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class viewController : MonoBehaviour {
+
+	[SerializeField]
+	private float xMax;
+	[SerializeField]
+	private float yMax;
+	[SerializeField]
+	private float xMin;
+	[SerializeField]
+	private float yMin;
+	[SerializeField]
+	private float speed;
+
+	private Transform target;
+	private GameObject myCamera;
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find ("Player").transform;
+		myCamera = GameObject.Find ("Main Camera");
+
+	}
+	void Update(){
+		yMin += speed * Time.deltaTime;
+		yMin = Mathf.Max(yMin,target.position.y);
+		transform.position = new Vector3(transform.position.x,transform.position.y + yMin,transform.position.z);
+	}
+	// Update is called once per frame
+	void LateUpdate () {
+		myCamera.transform.position = new Vector3 (Mathf.Clamp (target.position.x, xMin, xMax), myCamera.transform.position.y, myCamera.transform.position.z);
+		transform.position = new Vector3 (transform.position.x, Mathf.Clamp (target.position.y, yMin, yMax), transform.position.z);
+	}
+}
